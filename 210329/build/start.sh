79cf3e602d26ca0169fe8884a70604e149383445
Slick <holygalaxy444@gmail.com>
2021-04-05 07:11:48 +0500

    docker

---
+++ 210329/build/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Building kpk docker"
+docker build -f ./build/docker/Dockerfile -t kpk ./build/docker/
+
+echo "Starting kpk docker"
+docker run --rm -d \
+    -v $(pwd)/sources:/home/kpk/bin \
+    -v $(pwd)/data:/home/kpk/data \
+    -p 8027:8000 \
+    kpk
---
+++ 210329/sources/my_messenger/my_messenger/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('mainapp.urls', namespace='main')),
+
+    path('admin/', admin.site.urls),
+    path('accounts/',
+         include(('django.contrib.auth.urls', 'auth'), namespace='auth')),
+]
+
