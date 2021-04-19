2d17fdb7563f865dfc190be67b0ff70859722826
Slick <holygalaxy444@gmail.com>
2021-04-19 09:48:17 +0500

    docker

--- 210329/sources/my_messenger/mainapp/urls.py
+++ 210329/sources/my_messenger/mainapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+
 import mainapp.views as mainapp
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
-    path('dialog/<int:dialog_pk>/', mainapp.show_dialog, name='show_dialog'),
+    path('dialog/show/<int:dialog_pk>/', mainapp.dialog_show, name='dialog_show'),
+    path('dialog/create/', mainapp.dialog_create, name='dialog_create'),
+    path('user/dialog/create/<int:user_id>/', mainapp.user_dialog_create,
+         name='user_dialog_create'),
 ]
--- 210329/sources/my_messenger/mainapp/views.py
+++ 210329/sources/my_messenger/mainapp/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from mainapp.models import Dialog, DialogMemebers, Message
 
@@ -15,7 +18,7 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def show_dialog(request, dialog_pk):
+def dialog_show(request, dialog_pk):
     dialog = get_object_or_404(Dialog, pk=dialog_pk)
     _dialog_members = DialogMemebers.objects.filter(dialog=dialog)
     dialog_members = _dialog_members.exclude(member=request.user). \
@@ -24,10 +27,46 @@ def show_dialog(request, dialog_pk):
         select_related('sender__member')
 
     context = {
+        'page_title': 'диалог',
         'dialog': dialog,
         'dialog_members': dialog_members,
         'dialog_messages': dialog_messages,
     }
 
-    return render(request, 'mainapp/show_dialog.html', context)
+    return render(request, 'mainapp/dialog_show.html', context)
 
+
+def dialog_create(request):
+    dialogues = request.user.dialogs.select_related('dialog').all(). \
+        values_list('dialog_id', flat=True)
+    interlocutors = DialogMemebers.objects.filter(dialog__in=dialogues).\
+        values_list('member_id', flat=True)
+    new_interlocutors = User.objects.exclude(pk__in=interlocutors)
+
+    context = {
+        'page_title': 'новый диалог',
+        'new_interlocutors': new_interlocutors,
+    }
+    return render(request, 'mainapp/dialog_create.html', context)
+
+
+def user_dialog_create(request, user_id):
+    interlocutor = User.objects.get(pk=user_id)
+    dialog = Dialog.objects.create(
+        name=interlocutor.username
+    )
+    DialogMemebers.objects.create(
+        dialog=dialog,
+        member=request.user,
+        role=DialogMemebers.CREATOR
+    )
+    DialogMemebers.objects.create(
+        dialog=dialog,
+        # member_id=user_id,
+        member=interlocutor,
+        role=DialogMemebers.INTERLOCUTOR
+    )
+
+    return HttpResponseRedirect(
+        reverse('main:dialog_show', kwargs={'dialog_pk': dialog.pk})
+    )
