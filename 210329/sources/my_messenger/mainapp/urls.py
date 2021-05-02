70aeb0621d4fd3a6fa2cbda0300414246665a9ae
Slick <holygalaxy444@gmail.com>
2021-05-02 13:55:07 +0500

    message

--- 210329/sources/my_messenger/mainapp/urls.py
+++ 210329/sources/my_messenger/mainapp/urls.py
@@ -6,8 +6,16 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
+
     path('dialog/show/<int:dialog_pk>/', mainapp.dialog_show, name='dialog_show'),
+
     path('dialog/create/', mainapp.dialog_create, name='dialog_create'),
-    path('user/dialog/create/<int:user_id>/', mainapp.user_dialog_create,
-         name='user_dialog_create'),
+
+    path('user/dialog/create/<int:user_id>/', mainapp.user_dialog_create, name='user_dialog_create'),
+
+    path('user/dialog/delete/<int:pk>/', mainapp.dialog_delete, name='dialog_delete'),
+
+    path('dialog/member/<int:sender_pk>/message/create/',
+         mainapp.DialogMessageCreate.as_view(),
+         name='dialog_message_create'),
 ]
